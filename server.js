ec07e9a16e64028a51f8a7a059deff854d167427
rahulgorai123 <106511080+rahulgorai123@users.noreply.github.com>
2023-04-14 13:05:29 +0530

    final code
    ecommerce mern stack app

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from './routes/categoryRoutes.js'
+import productRoutes from "./routes/productRoutes.js"
+import cors from 'cors'
+import path from 'path'
+
+
+//configure env
+dotenv.config();
+//database config
+connectDB();
+
+const app=express();
+//middlewares
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+//routes
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/category",categoryRoutes);
+app.use("/api/v1/product",productRoutes);
+
+//rest api
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
+
+const port=process.env.port || 8000;
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`.bgCyan.white);
+})
